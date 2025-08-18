3a52f12ed85a285b7384df123e6c492636d88b68
Darren Cooney <darren.cooney@gmail.com>
2025-08-18 13:11:45 -0400

    Adding minified files

--- easy-anchor.php
+++ easy-anchor.php
@@ -17,62 +17,65 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-/**
- * Plugin initialization class
- */
-class Easy_Anchor_Plugin {
-    /**
-     * Constructor
-     */
-    public function __construct() {
-        $this->constants();
-        $this->actions();
-    }
-
-    /**
-     * Define plugin constants.
-     */
-    private function constants() {
-        define( 'EASY_ANCHOR_VERSION', '1.0.0' ); 
-    }
-
-    /**
-     * Initialize action hooks.
-     */
-    private function actions() {
-        add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_editor_assets' ] );
-    }
-
-    /**
-     * Enqueue editor assets.
-     */
-    public function enqueue_editor_assets() {
-        $asset_file = include( plugin_dir_path( __FILE__ ) . 'build' . '/editor.asset.php' );
-        
-        wp_enqueue_script(
-            'easy-anchor',
-            plugin_dir_url( __FILE__ ) . 'build' . '/editor.js',
-            $asset_file['dependencies'],
-            $asset_file['version']
-        );
-
-        wp_localize_script(
-            'easy-anchor',
-            'easyAnchor',
-            [
-                'pluginDir'   => plugin_dir_url( __FILE__ ), 
-                'supported'   => apply_filters( 'easy_anchor_supported', [ 
-                    'core/heading', 
-                    'core/paragraph' 
-                    ] 
-                ),
-                'maxLength'   => apply_filters( 'easy_anchor_max_length', 30 ),
-            ]
-        );
-
-        wp_set_script_translations( 'easy-anchor', 'easy-anchor' );
-    }
-}
 if ( ! class_exists( 'Easy_Anchor_Plugin' ) ) {
-    new Easy_Anchor_Plugin(); // Initialize plugin. 
+
+    /**
+     * Plugin initialization class
+     */
+    class Easy_Anchor_Plugin {
+        /**
+         * Constructor
+         */
+        public function __construct() {
+            $this->constants();
+            $this->actions();
+        }
+
+        /**
+         * Define plugin constants.
+         */
+        private function constants() {
+            define( 'EASY_ANCHOR_VERSION', '1.0.0' ); 
+        }
+
+        /**
+         * Initialize action hooks.
+         */
+        private function actions() {
+            add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_editor_assets' ] );
+        }
+
+        /**
+         * Enqueue editor assets.
+         */
+        public function enqueue_editor_assets() {
+            $suffix     = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min'; 
+            $asset_file = include( plugin_dir_path( __FILE__ ) . 'build' . '/editor' . $suffix . '.asset.php' );
+        
+            wp_enqueue_script(
+                'easy-anchor',
+                plugin_dir_url( __FILE__ ) . 'build' . '/editor' . $suffix . '.js',
+                $asset_file['dependencies'],
+                $asset_file['version']
+            );
+
+            wp_localize_script(
+                'easy-anchor',
+                'easyAnchor',
+                [
+                    'pluginDir'   => plugin_dir_url( __FILE__ ), 
+                    'supported'   => apply_filters( 'easy_anchor_supported', [ 
+                        'core/heading', 
+                        'core/paragraph' 
+                        ] 
+                    ),
+                    'maxLength'   => apply_filters( 'easy_anchor_max_length', 30 ),
+                ]
+            );
+
+            wp_set_script_translations( 'easy-anchor', 'easy-anchor' );
+        }
+    }
+
+    new Easy_Anchor_Plugin();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
-const path = require( 'path' );
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require( 'webpack-merge' );
 
 const config = {
 	entry: {
 		editor: path.resolve( __dirname, 'src/editor/index.js' ), 
-	},
-	output: {
-		path: path.resolve( __dirname, 'build' ), 
+	},	
+	optimization: {
+		minimize: false,
+		minimizer: [
+			new TerserPlugin({
+				extractComments: false,
+				test: /\.min.js(\?.*)?$/i,
+			}),
+		],
 	},
 };
 
